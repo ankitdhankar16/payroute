e373ff9db428938de25cbbb13a23d0a06f6cb161
Ankit Dhankar <adhankar1604@gmail.com>
2026-09-02 10:19:48 +0530

    chore: initialize project and configure PostgreSQL

---
+++ src/main/java/com/dhankarankit/payroute/PayrouteApplication.java
@@ -0,0 +1,13 @@
+package com.dhankarankit.payroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhankarankit/payroute/PayrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhankarankit.payroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
