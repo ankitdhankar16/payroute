2dae6af74f483c62fe7aba997d0131bf74a7fd9c
Ankit Dhankar <adhankar1604@gmail.com>
2026-09-04 22:31:59 +0530

    feat: app user entity

---
+++ src/main/java/com/dhankarankit/payroute/common/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.dhankarankit.payroute.common.enums;
+
+public enum UserRole {
+    OWNER,
+    ADMIN,
+    TEAM
+}
---
+++ src/main/java/com/dhankarankit/payroute/merchant/entity/AppUser.java
@@ -0,0 +1,34 @@
+package com.dhankarankit.payroute.merchant.entity;
+
+import com.dhankarankit.payroute.common.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "merchant_id")
+    private Merchant merchant;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+}
