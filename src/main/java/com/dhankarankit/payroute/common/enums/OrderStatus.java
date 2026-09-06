3c74fee3dafd5f3f21e6ca8c9ffbcafa48a17ec7
Ankit Dhankar <adhankar1604@gmail.com>
2026-09-06 22:03:51 +0530

    feat: OrderRecord Entity

---
+++ src/main/java/com/dhankarankit/payroute/common/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.dhankarankit.payroute.common.enums;
+
+public enum OrderStatus {
+    CREATED,
+    ATTEMPTED,
+    PAID,
+    CANCELLED
+}
---
+++ src/main/java/com/dhankarankit/payroute/payment/entity/OrderRecord.java
@@ -0,0 +1,44 @@
+package com.dhankarankit.payroute.payment.entity;
+
+import com.dhankarankit.payroute.common.entity.Money;
+import com.dhankarankit.payroute.common.enums.OrderStatus;
+import jakarta.persistence.*;
+import org.apache.tomcat.util.json.JSONFilter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name="order_record")
+public class OrderRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    //No FK cross-service boundary
+    @Column(name="merchant_id",nullable = false)
+    private UUID merchantId;
+
+    @Embedded
+    private Money amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private OrderStatus orderStatus = OrderStatus.CREATED;
+
+    @Column(nullable = false)
+    private Integer attempts=0;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Map<String,Object> notes;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+
+
+}
